e5fd5872dca908f08bae0b014dc62101153b08d2
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-03 12:33:42 -0400

    Initial commit

---
+++ ProblemSet3_Part1.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------
+# ProblemSet3_Part1.py
+#
+# Description: Problem Set 3 (Part 1)
+#
+# Author: Alison Stouffer (alison.stouffer@duke.edu)
+# Date:   Fall 2022
+#--------------------------------------------------------------
+
+#%% Code Block 1
+
+mountain = "Denali"
+nickname = 'Mt. McKinley'
+elevation == 20322 
+
+print (mountain + ", formerly known as "" + nickname + ",)
+print ()"is " + elevation + '' above sea level.' )

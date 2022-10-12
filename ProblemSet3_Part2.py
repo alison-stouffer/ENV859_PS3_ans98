82e29a586c2272d35241e483f983ea5c67578549
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-12 13:11:19 -0400

    Create ProblemSet3_Part2 project

---
+++ ProblemSet3_Part2.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ProblemSet3_Part2.py
+#
+# Description: Problem Set 3 (Part 2)
+#
+# Author: Alison Stouffer (alison.stouffer@duke.edu)
+# Date:   Fall 2022
+#--------------------------------------------------------------
+
+#%% Task 4: Lists, Dictionaries, String Manipulation, & Iteration

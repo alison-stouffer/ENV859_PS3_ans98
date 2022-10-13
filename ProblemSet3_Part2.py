a3f0244a0b785cf9e70271a0067d60ad710ca64e
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-13 11:16:53 -0400

    Update Task 5 code

--- ProblemSet3_Part2.py
+++ ProblemSet3_Part2.py
@@ -64,8 +64,8 @@ headerLineString = lineList[0]
 headerItems = headerLineString.split(sep=',')
 
 #Create empty latitude and longitude dictionaries:
-latDict = {}
-lonDict = {}
+latDict  = {}
+longDict = {}
 
 #Read each line of data:
 for lineString in lineList:
@@ -78,27 +78,33 @@ for lineString in lineList:
 
     #Extract items in list into variables:
     mmsi       = lineData[0]
-    start_lat  = lineData[1]
-    end_lat    = lineData[3]
-    start_long = lineData[2]
-
-
-
+    start_lat  = float(lineData[1])
+    end_lat    = float(lineData[3])
+    start_long = float(lineData[2])
 
     #Add start and end latitudes to dictionary, if criteria is met:
     if start_lat < 0 and end_lat > 0:
         latDict[mmsi] = (start_lat, end_lat)
-        #location_dict[record_id] = (obs_lat, obs_lon)
-    
-#Create an empty key list.
+    #Add start longitude to dictionary, if criteria is met:
+    if 165 < start_long < 170:
+        longDict[mmsi] = (start_long)
+ 
+#Create an empty key list:
 matching_keys = []
 
-# Loop through all key, value pairs in the date_dictionary.
-for the_key, the_value in date_dict.items():
-    #See if the date (the value) matches the user date.
-    if the_value == user_date:
+
+#use the value of the transmission_mmsi for the current line to query the vesselsDict created above to print the vessel’s mmsi and its fleet.
+
+#Extract intersecting keys in latitude and longitude dictionaries:
+for the_key in longDict:
+    if the_key in latDict:
         matching_keys.append(the_key)
-        
+
+
+
+
+#BONUS: If no vessels meet your criteria, print a message that states “No vessels met criteria”
+     
 # Report whether no keys were found.
 if len(matching_keys) == 0:
     print(f"Sara was not located on {user_date}")
@@ -108,10 +114,7 @@ for matching_key in matching_keys:
     obs_lat, obs_lon = location_dict[matching_key]
     print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {user_date}")
     
-#Examines the starting and ending latitude (the 2nd and 4th columns in the csv) to determine whether the event crosses the equator, passing from the southern hemisphere to the north.
 
-#Examines the starting longitude to see whether it falls between 165°E and 170°E. Again, it’s useful to create a Boolean variable that store whether this is true or false.
 
-#If both the latitude and longitude constraints are true, then use the value of the transmission_mmsi for the current line to query the vesselsDict created above to print the vessel’s mmsi and its fleet.
 
-#BONUS: If no vessels meet your criteria, print a message that states “No vessels met criteria”
+

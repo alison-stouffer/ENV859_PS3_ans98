e26b548398519cbb8f69274627614013caebe26c
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-12 21:18:44 -0400

    Create code for Task 4.2

--- ProblemSet3_Part2.py
+++ ProblemSet3_Part2.py
@@ -9,11 +9,20 @@
 
 #%% Task 4.1
 
-#Reading in the data.
 fileObj = open(file='transshipment_vessels_20180723.csv', mode='r')
 lineList = fileObj.readlines()
 fileObj.close()
 
-#Displaying column headers.
 headerLineString = lineList[0]
 print(headerLineString)
+
+#%% Task 4.2
+
+headerItems = headerLineString.split(sep=',')
+
+#List the index of the mmsi, shipname, and fleet_name values
+mmsi_idx  = headerItems.index('mmsi')
+name_idx  = headerItems.index('shipname')
+fleet_idx = headerItems.index('fleet_name')
+
+print(mmsi_idx,name_idx,fleet_idx)
